8a595ff6e5f8c766ada6608cd9d8d838e34ee997
Shayne Burgess <shayne.burgess@gmail.com>
2016-01-08 08:50:09 -0800

    Added new functionality for update/delete. Cleaned up verify

--- app/app.js
+++ app/app.js
@@ -7,35 +7,52 @@ qualcaddy.controller("AppCtrl", function ($http) {
     var app = this;
     var url = "https://qualcaddy.herokuapp.com/";
     //var url = "http://localhost:3000/"
+    var currentApplication = null;
 
-    app.saveApplication = function (newApplication) {
-        $http.post(url + "applications/add", {
-            name: newApplication.name,
-            income: newApplication.income,
-            debt: newApplication.debt
+    app.saveApplication = function () {
+        $http.post(url + "applications", {
+            name: app.currentApplication.name,
+            income: app.currentApplication.income,
+            debt: app.currentApplication.debt
         }).success(function () {
             loadApplications();
         })
     }
 
-    app.verify = function(newApplication, result){
-        $http.post(url + "verify", {
-            name: newApplication.name,
-            income: newApplication.income,
-            debt: newApplication.debt
-        }).success(function(res){
-            app.applicationResult = res;
+    app.deleteApplication = function(){
+        $http.delete(url + "applications/" + app.currentApplication._id,
+            app.currentApplication
+        ).success(function (res){
+            loadApplications();
         })
+    }
+
+    app.updateApplication = function(){
+        $http.post(url + "applications/" + app.currentApplication._id,
+            app.currentApplication
+        ).success(function (res){
+            loadApplications();
+        })
+    }
+
+    app.verify = function(){
+        if(app.currentApplication != null) {
+            $http.post(url + "verify", {
+                name: app.currentApplication.name,
+                income: app.currentApplication.income,
+                debt: app.currentApplication.debt
+            }).success(function (res) {
+                app.applicationResult = res;
+            })
+        }
     }
 
     function loadApplications() {
         $http.get(url + "applications").success(function(applications) {
             app.applications = applications;
+            app.currentApplication = app.applications[0];
+            app.verify();
         })
-        if(app.newApplication = null)
-        {
-            newApplication = app.applications[0];
-        }
     }
 
     loadApplications();
--- routes/data-routes.js
+++ routes/data-routes.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 
 var Applications = require('../models/applications');
 var ApplicationResults = require('../models/applicationresults');
@@ -14,7 +15,47 @@ module.exports = function(app) {
         })
     });
 
-    app.post("/applications/add", function(req, res) {
+    app.delete("/applications/:id", function(req, res){
+        console.log("Received request to delete id: " + req.params.id);
+        Applications.remove({ _id: req.params.id }, function(err) {
+            if (!err) {
+                console.log("Successfully deleted application with _id:" + req.params.id +
+                    " and name: " + req.body.name);
+                res.statusCode = 200;
+            }
+            else {
+                console.log("Failed to deleted application with _id:" + req.params.id +
+                    " and name: " + req.body.name);
+                req.statusCode = 500;
+            }
+        });
+        res.send();
+    });
+
+    app.post("/applications/:id", function(req, res){
+        console.log("Received request to edit application with id: " + req.params.id +
+                req
+        );
+        Applications.update(
+            { _id: req.params.id},
+            req.body,
+            {multi: false},
+            function(err, numAffected)
+            {
+               console.log("Successfully updated " + numAffected + " rows.");
+            }
+        );
+    });
+
+    app.post("/applications", function(req, res) {
+
+        if(!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('name'))
+        {
+            res.statusCode = 400;
+            return res.send('Error 400: Incorrect post syntax for creating an application. Missing properties.');
+        }
+
+
         var name = req.body.name;
         var income = req.body.income;
         var debt = req.body.debt;
@@ -25,14 +66,15 @@ module.exports = function(app) {
             debt: debt
         })
         console.log("Saving new record to DB: " +
-            application.name + ", " +
-            application.income + ", " +
-            application.debt
+                application.name + ", " +
+                application.income + ", " +
+                application.debt
         );
 
         application.save(function (err) {
-            res.send();
             console.log("Error saving record! " + err)
         })
+        res.statusCode = 200;
+        res.send("Request to add application accepted.");
     });
 };
